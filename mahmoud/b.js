9f57d8bf963daf239197cd8e19688d7a1ebc0fde
mahmoud15042006mahmoud <48564146+mahmoud15042006mahmoud@users.noreply.github.com>
2019-03-23 18:11:02 +0100

    Create b.js

---
+++ mahmoud/b.js
@@ -0,0 +1,13 @@
+<b:if cond='data:view.isPage'>
+<script type='text/javascript'>
+//<![CDATA[
+var textmonth = ["يناير","فبراير","مارس","أبريل","مايو","يونيو"," يوليو"," أغسطس","سبتمبر","أكتوبر","نوفمبر"," ديسمبر"];
+var noimg ="https://4.bp.blogspot.com/-OI0BoYP677M/VZu1TW7a3DI/AAAAAAAAFsE/4BRdE2YFS5A/s1600/noimg.png";
+var textscript ="مشاركة";
+!function(){var e=document.createElement("script")
+e.type="text/javascript",e.async=!0,e.src="https://cdn.jsdelivr.net/gh/abdelalilebbihi/abdou_tech/gadget/abdoutech-archive.js"
+var t=document.getElementsByTagName("script")[0]
+t.parentNode.insertBefore(e,t)}();
+//]]>
+</script>
+</b:if>
